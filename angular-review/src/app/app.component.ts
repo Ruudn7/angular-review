dfe1e117c1a573b777d363c42a104423593d4962
Sławomir <ruudn7@wp.pl>
2021-01-06 17:19:23 +0100

    simple counter function

--- angular-review/src/app/app.component.ts
+++ angular-review/src/app/app.component.ts
@@ -7,8 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lista zadań';
+  counter = 0;
+
+  constructor() {
+    setInterval( () => {}, 1000);
+  }
 
   get footer(): string {
     return '2020 © Lista zadań,All rights reserved.';
+  }
+
+  getInfo(): string {
+    console.log('wywołane');
+    return 'Tekst wyświetlony z metody';
+  }
+
+  count(): void {
+    this.counter++;
+  }
+
+  notBinded(): void {
+    console.log('Ta metoda nie jest bindowana');
   }
 }
