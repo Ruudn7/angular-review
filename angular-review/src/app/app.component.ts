6dcb8f247c847ab1534e3b6c86897c64da0d86fa
Sławomir <ruudn7@wp.pl>
2021-01-06 18:01:06 +0100

    question mark and change data structure

--- angular-review/src/app/app.component.ts
+++ angular-review/src/app/app.component.ts
@@ -6,27 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Lista zadań';
-  counter = 0;
+  config: { [key: string]: string } = {};
 
   constructor() {
-    setInterval( () => {}, 1000);
-  }
+    setTimeout( () => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: ' © Lista zadań,All rights reserved.',
+        date: new Date().toDateString()
+      };
+    }, 500);
 
-  get footer(): string {
-    return '2020 © Lista zadań,All rights reserved.';
-  }
-
-  getInfo(): string {
-    console.log('wywołane');
-    return 'Tekst wyświetlony z metody';
-  }
-
-  count(): void {
-    this.counter++;
-  }
-
-  notBinded(): void {
-    console.log('Ta metoda nie jest bindowana');
   }
 }
